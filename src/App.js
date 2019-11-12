989fedede8249880e092d29f2ddafab6791d748f
Baruch Elias <eyesweyedopen@gmail.com>
2019-11-12 18:40:55 -0500

    re-edited p tag

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
             <img src = { logo }
                 className = "App-logo"
                 alt = "logo" / >
-            <p>Edit <code> src / App.js </code> and save to reload. </p> 
+            <p>Edit <code> src / App.js </code> </p> 
             <a className = "App-link"
                 href = "https://reactjs.org"
                 target = "_blank"
